1468968a27f802adeefdb396be1c6776915fa51d
cyberalexix <alexix2015@gmail.com>
2023-01-13 07:12:12 +0200

    Updated migration query, now it returns an empty jsonb in case hour column equals null

--- migrations/models/8_20230109020500_change_user_hour_from_smallint_to_json.sql
+++ migrations/models/8_20230109020500_change_user_hour_from_smallint_to_json.sql
@@ -1,4 +1,4 @@
 -- upgrade --
-ALTER TABLE "user" ALTER COLUMN "hour" TYPE JSONB USING json_build_array("hour");
+ALTER TABLE "user" ALTER COLUMN "hour" TYPE JSONB USING CASE WHEN "hour" IS NULL THEN jsonb_build_array() ELSE jsonb_build_array("hour") END;
 -- downgrade --
 ALTER TABLE "user" ALTER COLUMN "hour" TYPE SMALLINT USING NULLIF("hour"[0], 'null')::SMALLINT;
